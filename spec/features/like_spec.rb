b250aed0e6af5311d1a4f8b88273a9b85fbba85c
Caitlin Cooling <ccooling97@gmail.com>
2018-11-27 16:30:01 +0000

    Viewing like count: - User can only like a post once - 'Like' is pluralized where neccessary

--- spec/features/like_spec.rb
+++ spec/features/like_spec.rb
@@ -11,8 +11,16 @@ feature 'Likes' do
   end
 
   scenario 'A user can like a post' do
-    # save_and_open_page
     within '.all-posts' do
+      click_button 'Like'
+    end
+    expect(page).to have_content '1 like'
+  end
+
+  scenario 'A user can only like a post once' do
+    within '.all-posts' do
+      click_button 'Like'
+      click_button 'Like'
       click_button 'Like'
     end
     expect(page).to have_content '1 like'
