34f41405487069b6c4a3588b103c41659219eced
Caitlin Cooling <ccooling97@gmail.com>
2018-11-30 09:50:44 +0000

    Show image from profile - Click on an image from the profile page for more info - Removed broken back button - Show page shows post image, caption, likes, comments with the option to edit or delete - Currently these buttons are redirecting to the newsfeed - Capybara click image is not currently working

--- spec/features/profile_page_spec.rb
+++ spec/features/profile_page_spec.rb
@@ -6,17 +6,35 @@ feature 'Profile page' do
 
   before do
     sign_up
+    create_post
+    write_comment
+    click_link 'Profile'
   end
 
   scenario 'has name and avatar on it' do
-    click_link 'Profile'
     expect(page).to have_content 'Caitlin Cooling'
     expect(page.find('#user')['src']).to have_content 'profile1.jpg'
   end
 
   scenario 'has the users posts on it' do
-    create_post
-    click_link 'Profile'
     expect(page.find('.post-image')['src']).to have_content "image.png"
+  end
+
+  feature 'user can click on the image for more options'
+
+  before do
+    find(:css, '.post-image').click
+  end
+
+  scenario 'the see the caption, likes count and comments' do
+    expect(page).to have_content 'This is so cool'
+    expect(page).to have_content '0 likes'
+  end
+
+  scenario 'user can like, comment, edit and delete the post' do
+    expect(page).to have_link '❤️'
+    expect(page).to have_link '💬'
+    expect(page).to have_link '✏️'
+    expect(page).to have_link '🗑️'
   end
 end
